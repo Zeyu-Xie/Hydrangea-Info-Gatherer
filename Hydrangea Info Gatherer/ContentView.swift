86d386b7335671cc1bbff84b8eccd7891066358e
Acan <xie.zeyu20@gmail.com>
2023-09-26 13:00:50 +0800

    .

--- Hydrangea Info Gatherer/ContentView.swift
+++ Hydrangea Info Gatherer/ContentView.swift
@@ -12,7 +12,18 @@ struct ContentView: View {
     @State private var content = ""
     @State private var source = "https://zeyu-xie.github.io/Hydrangea-Info-Gatherer/data/Baidu_Hot_Search/latest.txt"
     
-    let pattern = "http.*?sa=fyb_news&rsv_dl=fyb_news"
+    let num_pattern="- No.*? -"
+    let title_pattern="- Title.*? -"
+    let index_pattern="- Index.*? -"
+    let content_pattern="- Content.*? -"
+    let url_pattern = "- URL.*? -"
+    
+    @State private var num_contents: Array<String> = []
+    @State private var title_contents: Array<String> = []
+    @State private var index_contents: Array<String> = []
+    @State private var content_contents: Array<String> = []
+    @State private var url_contents: Array<String> = []
+    @State private var count:Int = 0
     
     var body: some View {
         VStack {
@@ -23,6 +34,28 @@ struct ContentView: View {
         .padding()
         .onAppear {
             getLatestNews()
+        }
+        VStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ForEach(0..<count, id: \.self) { num in
+                        
+                        let _num = num_contents[num].dropFirst(6).dropLast(2)
+                        
+                        let _title = title_contents[num].dropFirst(9).dropLast(2)
+                        
+                        let _index = index_contents[num].dropFirst(9).dropLast(2)
+                        
+                        let _content = content_contents[num].dropFirst(11).dropLast(2)
+                        
+                        let _url = url_contents[num].dropFirst(7).dropLast(2)
+                                            
+                        Link(_num+". "+_title, destination: URL(string: String(_url))!)
+                        Text(_content)
+                        Text("")
+                    }
+                }
+            }
         }
     }
     
@@ -35,26 +68,13 @@ struct ContentView: View {
                 } else if let data = data {
                     if let responseString = String(data: data, encoding: .utf8) {
                         DispatchQueue.main.async {
-                            
-                            do {
-                                content=responseString
-                                
-                                let regex = try NSRegularExpression(pattern: pattern, options: [])
-                                let range = NSRange(content.startIndex..<content.endIndex, in: content)
-                                
-                                let matches = regex.matches(in: content, options: [], range: range)
-                                
-                                for match in matches {
-                                    let matchedRange = Range(match.range, in: content)!
-                                    let matchedSubstring = content[matchedRange]
-                                    print("找到的子字符串是：\(matchedSubstring)")
-                                }
-                                
-                                
-                                
-                            } catch {
-                                print("ERROR：\(error)")
-                            }
+                            num_contents = fitPattern(res: responseString, pattern: num_pattern)
+                            title_contents=fitPattern(res: responseString, pattern: title_pattern)
+                            index_contents=fitPattern(res: responseString, pattern: index_pattern)
+                            content_contents=fitPattern(res: responseString, pattern: content_pattern)
+                            url_contents=fitPattern(res: responseString, pattern: url_pattern)
+                            count=num_contents.count
+                            print(type(of: num_contents[0]))
                         }
                     }
                 }
@@ -62,6 +82,32 @@ struct ContentView: View {
             task.resume()
         }
     }
+    
+    func fitPattern (res: String, pattern: String) -> [String] {
+        do {
+            content=res
+            
+            let regex = try NSRegularExpression(pattern: pattern, options: [])
+            let range = NSRange(content.startIndex..<content.endIndex, in: content)
+            
+            let matches = regex.matches(in: content, options: [], range: range)
+            
+            var result: [String] = []
+            
+            for match in matches {
+                let matchedRange = Range(match.range, in: content)!
+                let matchedSubstring = content[matchedRange]
+                result.append(String(matchedSubstring))
+            }
+            
+            return result
+            
+        } catch {
+            print("ERROR：\(error)")
+            return []
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
