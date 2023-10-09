926935937aee761f77d863c1d792191595c5d6c0
Acan <xie.zeyu20@gmail.com>
2023-10-09 21:15:49 +0800

    finished

--- Hydrangea Info Gatherer/ContentView.swift
+++ Hydrangea Info Gatherer/ContentView.swift
@@ -14,114 +14,45 @@
 
 import SwiftUI
 
+struct FirstView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                BaiduNews()
+            }
+        }
+    }
+}
+
+struct SecondView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                About()
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     
-    @State private var content = ""
-    @State private var source = "https://zeyu-xie.github.io/Hydrangea-Info-Gatherer/data/Baidu_Hot_Search/latest.txt"
-    
-    let num_pattern="- No.*? -"
-    let title_pattern="- Title.*? -"
-    let index_pattern="- Index.*? -"
-    let content_pattern="- Content.*? -"
-    let url_pattern = "- URL.*? -"
-    
-    @State private var num_contents: Array<String> = []
-    @State private var title_contents: Array<String> = []
-    @State private var index_contents: Array<String> = []
-    @State private var content_contents: Array<String> = []
-    @State private var url_contents: Array<String> = []
-    @State private var count:Int = 0
-    
-    
-    
+
     var body: some View {
         
-        VStack {
-            
-            ScrollView(showsIndicators: false) {
-                
-                VStack(alignment: .leading) {
-                    Text("Hydrangea Info Gatherer").font(.largeTitle).padding(.bottom)
-                    Link(destination: URL(string: "https://zeyu-xie.github.io/Acan")!) {
-                        Text("- Made by Acan")
-                    }.padding(.bottom)
-                    Link(destination: URL(string: "https://github.com/zeyu-xie/Hydrangea-Info-Gatherer/tree/iOS")!) {
-                        Text("- GitHub")
-                    }
-                    Divider()
-                }.frame(alignment: .leading)
-                
-                VStack(alignment: .leading) {
-                    ForEach(0..<count, id: \.self) { num in
-                        
-                        let _num = num_contents[num].dropFirst(6).dropLast(2)
-                        
-                        let _title = title_contents[num].dropFirst(9).dropLast(2)
-                        
-                        let _index = index_contents[num].dropFirst(9).dropLast(2)
-                        
-                        let _content = content_contents[num].dropFirst(11).dropLast(2)
-                        
-                        let _url = url_contents[num].dropFirst(7).dropLast(2)
-                                            
-                        Link(_num+". "+_title, destination: URL(string: String(_url))!).padding(.bottom)
-                        Text(_content)
-                        Divider()
-                    }
-                }.padding(.top)
+        TabView {
+            FirstView().tabItem {
+                Image(systemName: "globe.asia.australia")
+                Text("Baidu News")
             }
-        }.padding().onAppear() {
-            getLatestNews()
+            SecondView().tabItem {
+                Image(systemName: "person")
+                Text("About")
+            }
+            
         }
+        
     }
     
-    func getLatestNews() {
-        if let url = URL(string: source) {
-            let session = URLSession.shared
-            let task = session.dataTask(with: url) { (data, response, error) in
-                if let error = error {
-                    print("ERROR：\(error)")
-                } else if let data = data {
-                    if let responseString = String(data: data, encoding: .utf8) {
-                        DispatchQueue.main.async {
-                            num_contents = fitPattern(res: responseString, pattern: num_pattern)
-                            title_contents=fitPattern(res: responseString, pattern: title_pattern)
-                            index_contents=fitPattern(res: responseString, pattern: index_pattern)
-                            content_contents=fitPattern(res: responseString, pattern: content_pattern)
-                            url_contents=fitPattern(res: responseString, pattern: url_pattern)
-                            count=num_contents.count
-                        }
-                    }
-                }
-            }
-            task.resume()
-        }
-    }
-    
-    func fitPattern (res: String, pattern: String) -> [String] {
-        do {
-            content=res
-            
-            let regex = try NSRegularExpression(pattern: pattern, options: [])
-            let range = NSRange(content.startIndex..<content.endIndex, in: content)
-            
-            let matches = regex.matches(in: content, options: [], range: range)
-            
-            var result: [String] = []
-            
-            for match in matches {
-                let matchedRange = Range(match.range, in: content)!
-                let matchedSubstring = content[matchedRange]
-                result.append(String(matchedSubstring))
-            }
-            
-            return result
-            
-        } catch {
-            print("ERROR：\(error)")
-            return []
-        }
-    }
     
 }
 
---
+++ Hydrangea Info Gatherer/components/About.swift
@@ -0,0 +1,92 @@
+//
+//  About.swift
+//  Hydrangea Info Gatherer
+//
+//  Created by Acan on 2023/10/9.
+//
+
+import SwiftUI
+
+struct About: View {
+    var body: some View {
+        ScrollView
+        {
+            VStack {
+            VStack(alignment: .leading, spacing: 16) {
+                
+                Divider()
+                
+                Text("Welcome to Hydrangea Info Gatherer, your go-to source for the latest updates and insights from Baidu Hot Searches. Created by Acan on September 26, 2023, this SwiftUI app provides a streamlined and informative way to stay updated on the trending topics.")
+                    .font(.body)
+            }.padding(.bottom)
+            
+            VStack(alignment: .leading, spacing: 16) {
+                
+                Divider()
+                
+                Text("Features:")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                
+                Text("- Baidu Hot Searches: Hydrangea Info Gatherer fetches real-time data from Baidu's Hot Searches, ensuring you're always in the loop about what's capturing the online world's attention.")
+                    .font(.body)
+                
+                Text("- Structured Presentation: The app organizes the information into distinct categories, including news number, title, index, content, and URL. This structured presentation makes it easy for you to grasp the key details at a glance.")
+                    .font(.body)
+                
+                Text("- Dynamic Content: As the app dynamically retrieves the latest news, you can trust that the content remains fresh and relevant. The information is presented in a scrollable format for a seamless reading experience.")
+                    .font(.body)
+            }.padding(.bottom)
+            
+            VStack(alignment: .leading, spacing: 16) {
+                
+                Divider()
+                
+                Text("How to Use:")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                
+                Text("1. Launch the app to access the latest Baidu Hot Searches.")
+                    .font(.body)
+                
+                Text("2. Scroll through the content to explore various topics.")
+                    .font(.body)
+                
+                Text("3. Click on the provided links to delve deeper into articles that pique your interest.")
+                    .font(.body)
+            }.padding(.bottom)
+            
+            VStack(alignment: .leading, spacing: 16) {
+                
+                Divider()
+                
+                Text("Technical Details:")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                
+                Text("- Source URL: The app pulls data from the source URL: https://zeyu-xie.github.io/Hydrangea-Info-Gatherer/data/Baidu_Hot_Search/latest.txt")
+                    .font(.body)
+                
+                Text("- Regex Pattern Matching: The app utilizes regular expressions (regex) to extract specific information from the source data, ensuring accurate and structured content presentation.")
+                    .font(.body)
+            }.padding(.bottom)
+            
+                VStack(alignment: .leading, spacing: 16) {
+                    
+                    Divider()
+                    
+                    Text("Developer:")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    
+                    Text("- Creator: Acan")
+                        .font(.body)
+                    
+                    Text("- Creation Date: October 9, 2023")
+                        .font(.body)
+                }
+            }.padding()
+        }
+        .navigationBarTitle("About")
+    }
+}
